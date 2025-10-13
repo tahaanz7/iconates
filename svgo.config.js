e15d976de59d5f9667a3e13ce6fc604abbd6bcb6
Taha Anz <tahaanz70@gmail.com>
2025-10-13 18:41:14 +0100

    update optimzing script

--- svgo.config.js
+++ svgo.config.js
@@ -1,29 +1,41 @@
 export default {
   multipass: true,
   plugins: [
-    { name: 'removeDimensions', active: true },
+   
+    { name: 'cleanupAttrs', active: true },
+    { name: 'removeDimensions', active: false },
     { name: 'collapseGroups', active: true },
     { name: 'mergePaths', active: true },
+    { name: 'convertShapeToPath', active: true },
+    { name: 'convertPathData', active: true },
+    { name: 'removeUselessDefs', active: true },
     { name: 'removeUselessStrokeAndFill', active: true },
+
+   
     {
       name: 'removeAttrs',
       params: {
         attrs: ['data-name', 'id', 'class', 'style', 'stroke', 'fill']
       }
     },
+
+    
     { name: 'removeViewBox', active: false },
+
+
     {
       name: 'addAttributesToSVGElement',
       params: {
         attributes: [
           { xmlns: 'http://www.w3.org/2000/svg' },
+          { width: '24' },
+          { height: '24' },
+          { viewBox: '0 0 24 24' },
           { fill: 'none' },
           { stroke: 'currentColor' },
           { 'stroke-width': '2' },
           { 'stroke-linecap': 'round' },
-          { 'stroke-linejoin': 'round' },
-          { 'stroke-miterlimit': '10' },
-          { viewBox: '0 0 24 24' }
+          { 'stroke-linejoin': 'round' }
         ]
       }
     }
